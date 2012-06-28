74e1e2712b4d578b7fa77451eb1d4f0d78eea7c4
Wu Jiang <wujiang27@gmail.com>
2012-06-28 11:49:25 -0400

    upgraded emacs

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -23,7 +23,7 @@ alias sl=ls # often screw this up
 alias afind='ack-grep -il'
 
 alias EED="SUDO_EDITOR=\"emacsclient -nw -n -c -a emacs\" sudoedit"
-alias edmn="/usr/local/Cellar/emacs/HEAD/Emacs.app/Contents/MacOS/Emacs --daemon"
+alias edmn="/usr/local/Cellar/emacs/24.1/Emacs.app/Contents/MacOS/Emacs --daemon"
 alias eed="emacsclient -t -c -a emacs $@"
 
 # Nosetests
